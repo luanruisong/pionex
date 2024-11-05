c07c3079f4e4f9825a13cbeaf3291d5a7f11878e
Luan Ruisong <luanruisong@bituniverse.org>
2024-11-05 14:42:50 +0800

    fix form encoder

--- api/utils.go
+++ api/utils.go
@@ -1,11 +1,10 @@
 package api
 
 import (
-	"fmt"
+	"github.com/go-playground/form/v4"
 	jsoniter "github.com/json-iterator/go"
 	"github.com/valyala/fasthttp"
 	"net/http"
-	"reflect"
 )
 
 const (
@@ -15,37 +14,27 @@ const (
 	Host    = "https://api.pionex.com"
 )
 
+var (
+	formEncoder *form.Encoder
+)
+
+func init() {
+	formEncoder = form.NewEncoder()
+}
+
 func (a *Api[Req, Res]) preParseRequest(req *fasthttp.Request, data any) {
 	req.Header.SetMethod(a.Method)
 	req.SetRequestURI(Host)
 	u := req.URI()
 	u.SetPath(a.Path)
 	switch a.Method {
-	case http.MethodPost, http.MethodPut:
+	case http.MethodGet:
+		q, _ := formEncoder.Encode(data)
+		if len(q) > 0 {
+			u.SetQueryString(q.Encode())
+		}
+	case http.MethodPost, http.MethodPut, http.MethodDelete:
 		b, _ := jsoniter.Marshal(data)
 		req.SetBody(b)
-		return
-	}
-	v := reflect.ValueOf(data)
-	switch v.Kind() {
-	case reflect.Ptr:
-		if v.IsNil() {
-			return
-		}
-		v = v.Elem()
-	case reflect.Invalid:
-		return
-	}
-	if !v.IsZero() {
-		arg := u.QueryArgs()
-		for i := 0; i < v.NumField(); i++ {
-			field := v.Type().Field(i)
-			fv := fmt.Sprintf("%v", v.Field(i).Interface())
-			if tag := field.Tag.Get("form"); len(tag) > 0 {
-				arg.Add(tag, fv)
-				continue
-			}
-			arg.Add(field.Name, fv)
-		}
 	}
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,8 @@ require github.com/json-iterator/go v1.1.12
 
 require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/go-playground/form/v4 v4.2.1 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
